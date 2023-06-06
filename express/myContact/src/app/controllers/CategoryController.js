4c6f2c6e3ccbb5af6e3795be20eb92d3885bd538
guusfelix2015 <gusfelix2015@gmail.com>
2023-06-06 08:09:16 -0300

    create methods categories

--- express/myContact/src/app/controllers/CategoryController.js
+++ express/myContact/src/app/controllers/CategoryController.js
@@ -16,6 +16,43 @@ class CategoryController {
     const category = await CategoryRepository.create({ name });
     response.json(category);
   }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await CategoryRepository.findById(id);
+    if (!category) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    const categoryExists = await CategoryRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoryRepository.update(id, { name });
+    return response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoryRepository.delete(id);
+    // 204: No Content
+    response.sendStatus(204);
+  }
 }
 
 module.exports = new CategoryController();
