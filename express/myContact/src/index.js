2a1bb65dc0e0bc2a0234a11dd8e42e1ccce4e598
guusfelix2015 <gusfelix2015@gmail.com>
2023-06-07 08:43:59 -0300

    set error handler

--- express/myContact/src/index.js
+++ express/myContact/src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,6 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('########### Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
