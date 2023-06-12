11988072063511a8e5d1a6745d3fe687fc69fc75
guusfelix2015 <gusfelix2015@gmail.com>
2023-06-12 14:19:46 -0300

    expreesions jsx

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+const category = "Posts da semana";
+
 function App() {
-  return <h1>Hello World!!!</h1>;
+  return (
+    <>
+      <h1>JSstack's blog</h1>
+      <h2>{category}</h2>
+    </>
+  );
 }
 
 export default App;
