fd6737e98bd18af18078d6509a05752c52043212
guusfelix2015 <gusfelix2015@gmail.com>
2023-06-12 15:09:55 -0300

    adding header

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 import Post from "./Post";
+import Header from "./Header";
 
 function App() {
   return (
     <>
-      <h1>JSstack's blog</h1>
-      <h2>Posts da semana</h2>
-
+      <Header title="Post da semana" />
       <hr />
       <Post title="Título da notícia 01" subtitle="Subtítulo da notícia 01" />
       <Post title="Título da notícia 02" subtitle="Subtítulo da notícia 02" />
