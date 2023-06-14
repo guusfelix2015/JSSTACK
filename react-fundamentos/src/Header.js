17a62ba3a695f0286eac4fb622603944e263fa7b
guusfelix2015 <gusfelix2015@gmail.com>
2023-06-14 09:04:26 -0300

    props

--- react-fundamentos/src/Header.js
+++ react-fundamentos/src/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function Header({ title }) {
+export default function Header({ title, children }) {
   return (
     <>
       <h1>{title}</h1>
-      <h2>Posts da semana</h2>
+      {children}
     </>
   );
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -1,13 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function Post({ title, subtitle }) {
+export default function Post({ title, subtitle, likes }) {
   return (
     <>
       <article>
         <h3>{title}</h3>
         <small>{subtitle}</small>
+        <br />
+        Media: {likes / 2}
       </article>
       <br />
     </>
   );
 }
+
+Post.propTypes = {
+  likes: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+};
